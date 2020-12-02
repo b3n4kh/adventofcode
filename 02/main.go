e85388aae660726a52c73cfe1037f774b38c349a
b3n4kh <b@akhras.at>
2020-12-02 20:46:55 +0100

    new password type

--- 02/main.go
+++ 02/main.go
@@ -15,30 +15,36 @@ func check(e error) {
 	}
 }
 
-func parseFields(fields []string) (minKey int, maxKey int, key string, password string) {
-	keyRange := strings.Split(fields[0], "-")
-	minKey, err := strconv.Atoi(keyRange[0])
-	maxKey, err = strconv.Atoi(keyRange[1])
-	check(err)
-	key = string(fields[1][0])
-	password = fields[2]
-
-	return minKey, maxKey, key, password
+// Password after parsing input
+type Password struct {
+	minKey   int
+	maxKey   int
+	letter   string
+	password string
 }
 
-func checkPassword(fields []string) int {
-	minKey, maxKey, key, password := parseFields(fields)
-	keyCount := strings.Count(password, key)
-	if keyCount >= minKey && keyCount <= maxKey {
+func parseFields(fields []string) Password {
+	keyRange := strings.Split(fields[0], "-")
+	minKey, err := strconv.Atoi(keyRange[0])
+	maxKey, err := strconv.Atoi(keyRange[1])
+	check(err)
+	key := string(fields[1][0])
+	password := fields[2]
+
+	return Password{minKey, maxKey, key, password}
+}
+
+func checkPassword(password Password) int {
+	keyCount := strings.Count(password.password, password.letter)
+	if keyCount >= password.minKey && keyCount <= password.maxKey {
 		return 1
 	}
 
 	return 0
 }
 
-func checkPassword2(fields []string) int {
-	minKey, maxKey, key, password := parseFields(fields)
-	if (string(password[minKey-1]) == key) != (string(password[maxKey-1]) == key) {
+func checkPassword2(password Password) int {
+	if (string(password.password[password.minKey-1]) == password.letter) != (string(password.password[password.maxKey-1]) == password.letter) {
 		return 1
 	}
 
@@ -48,13 +54,13 @@ func checkPassword2(fields []string) int {
 func main() {
 	dat, err := ioutil.ReadFile("input.txt")
 	check(err)
-	var result1 int
-	var result2 int
+	var result1, result2 int
 	scanner := bufio.NewScanner(bytes.NewReader(dat))
 	for scanner.Scan() {
 		fields := strings.Split(scanner.Text(), " ")
-		result1 = result1 + checkPassword(fields)
-		result2 = result2 + checkPassword2(fields)
+		password := parseFields(fields)
+		result1 = result1 + checkPassword(password)
+		result2 = result2 + checkPassword2(password)
 	}
 	fmt.Printf("Number of Matches for 1: %d\n", result1)
 	fmt.Printf("Number of Matches for 2: %d", result2)
