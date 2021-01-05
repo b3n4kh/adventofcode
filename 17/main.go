5a7b633c287adcfec9708843935dac38ba039856
Benjamin Akhras <b@akhras.at>
2021-01-05 02:09:12 +0100

    18

--- 17/main.go
+++ 17/main.go
@@ -6,16 +6,38 @@ import (
 	"os"
 )
 
+type layer [][]int
+
+func cycle() {
+
+}
+
 func main() {
-	dat, err := os.Open("input.txt")
+	dat, err := os.Open("test.txt")
 	if err != nil {
 		panic(err)
 	}
 	defer dat.Close()
 	scanner := bufio.NewScanner(dat)
 	var line string
+	grid := make(map[int]layer)
+	baseLayer := layer{}
+	yPos := 0
 	for scanner.Scan() {
 		line = scanner.Text()
-		fmt.Println(line)
+		baseLayer = append(baseLayer, []int{})
+		baseLayer[yPos] = make([]int, len(line))
+		for i, char := range line {
+			if char == '.' {
+				baseLayer[yPos][i] = 0
+			}
+			if char == '#' {
+				baseLayer[yPos][i] = 1
+			}
+		}
+		yPos++
 	}
+	grid[0] = baseLayer
+	fmt.Printf("BaseState: %+v\n", grid)
+
 }
--- 18/main.go
+++ 18/main.go
@@ -4,18 +4,93 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
+func parseParanthesis(elements []string) (subElements map[int]string) {
+	var depth, lastDepth, subBlock, diff int
+	var numOrOp string
+	subElements = make(map[int]string)
+
+	for _, e := range elements {
+		for _, char := range e {
+			switch char {
+			case '(':
+				depth++
+			case ')':
+				depth--
+			default:
+				numOrOp = string(char)
+			}
+		}
+		//fmt.Printf("depth: %v, lastDepth: %v, numOrOp: %v\n", depth, lastDepth, numOrOp)
+		diff = depth - lastDepth
+		lastDepth = depth
+
+		if diff > 0 {
+			subBlock += diff
+			subElements[subBlock] += numOrOp
+		} else {
+			subElements[subBlock] += numOrOp
+			subBlock += diff
+		}
+	}
+	return subElements
+}
+
+func calculate(line string) (result int) {
+	line = strings.ReplaceAll(line, "(", "( ")
+	line = strings.ReplaceAll(line, ")", " )")
+
+	elements := strings.Split(line, " ")
+	//subElements := parseParanthesis(elements)
+	var numberL, numberR, depth int
+	var operator rune
+	for i, e := range elements {
+
+		for _, char := range e {
+			switch char {
+			case '(':
+				depth++
+			case ')':
+				depth--
+			case '*', '+':
+				operator = char
+			default:
+				if numberL == 0 {
+					numberL, _ = strconv.Atoi(string(char))
+					continue
+				}
+				numberR, _ = strconv.Atoi(string(char))
+			}
+		}
+		if numberL != 0 && numberR != 0 {
+			if operator == '+' {
+				result += numberL + numberR
+			}
+			if operator == '*' {
+				result += numberL * numberR
+			}
+		}
+		fmt.Printf("Index: %v, Element: %v\n", i, e)
+
+	}
+	return result
+}
+
 func main() {
-	dat, err := os.Open("input.txt")
+	dat, err := os.Open("test.txt")
 	if err != nil {
 		panic(err)
 	}
 	defer dat.Close()
 	scanner := bufio.NewScanner(dat)
 	var line string
+	var result int
 	for scanner.Scan() {
 		line = scanner.Text()
-		fmt.Println(line)
+		result += calculate(line)
 	}
+	fmt.Printf("Result1: %v\n", result)
 }
